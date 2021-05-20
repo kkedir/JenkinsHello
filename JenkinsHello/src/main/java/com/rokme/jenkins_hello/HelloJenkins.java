d7d56dfffe43742e7627a160effb213fa4296a63
korme kedir <korme6868@gmail.com>
2021-05-19 17:40:32 -0700

    Initial commit

---
+++ JenkinsHello/src/main/java/com/rokme/jenkins_hello/HelloJenkins.java
@@ -0,0 +1,19 @@
+package com.rokme.jenkins_hello;
+
+public class HelloJenkins {
+
+	public static void main(String[] args) {
+
+		System.out.println(helloJenkins());
+		System.out.println(helloJenkinsAgain());
+
+	}
+	
+	public static String helloJenkins() {
+		return "Hello Jenkins!";
+	}
+	public static String helloJenkinsAgain() {
+		return "Hello Jenkins again!!";
+	}
+
+}
